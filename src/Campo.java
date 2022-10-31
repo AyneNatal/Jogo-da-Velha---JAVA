a205ea6b164b00331103fb0a31825903244745d7
Lorrayne Natal <loyne.dev@gmail.com>
2022-10-31 16:24:00 -0300

    criando as variaveis

---
+++ src/Campo.java
@@ -0,0 +1,20 @@
+public class Campo {
+    private char simbolo;
+
+    public Campo() {
+        this.simbolo = ' '; //inicia com vazio
+    }
+
+    //getters & setters
+    public char getSimbolo() {
+        return simbolo;
+    }
+
+    public void setSimbolo(char simbolo) {
+        if (this.simbolo == ' ') {
+            this.simbolo = simbolo;
+        } else {
+            System.out.println("Campo já usado!");
+        }
+    }
+}
---
+++ src/JogoDaVelha.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class JogoDaVelha {
+    public static void main(String[] args) {
+        Campo[][] tabuleiro = new Campo[3][3];
+        Scanner scan = new Scanner(System.in);
+        char simboloAtual = 'X'; //todo implementar um random aqui
+        String vitoria = "";
+        boolean gameON = true;
+
+        while (gameON) {
+            //
+        }
+    }
+
+    public static void desenhaJogo (Campo[][] tabuleiro) {
+        //
+    }
+}
